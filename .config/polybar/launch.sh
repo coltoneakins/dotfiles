d02508792ef090257194b5126ba7f7b806cedb5b
Colton Eakins <coltoneakins@gmail.com>
2018-11-24 14:18:54 -0600

    Remove symlinks and unused bash shell scripts.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,1 +1,13 @@
-../../applications/dotfiles/local/.local/bin/barstart
+#!/usr/bin/env fish
+
+killall -q polybar
+
+
+while pgrep -x polybar >/dev/null
+    sleep 2
+end
+
+# Launch top bar
+polybar --config=$HOME/.config/polybar/config top 2> /dev/null &
+sleep 2
+polybar --config=$HOME/.config/polybar/config bottom 2> /dev/null &
